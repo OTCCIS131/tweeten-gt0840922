97ef0edc0daf497885c9b08ed9a3abac190b3c4c
Garrett Troy <gt0840922@otc.edu>
2017-10-04 13:11:21 -0500

    My Tweeten Page Part 3

--- main.js
+++ main.js
@@ -1,16 +1,18 @@
 $(function() {
     $('.btn-inc').click(({target}) => {
-        console.log(target)
-        let currentCount = $(target).next().text()
-        console.log(currentCount)
-        //console.log(this)
+        let countTag = $(target).next().text()
+        console.log(countTag)
+        // let currentCount = $(target).next().text()
+        // console.log(currentCount)
     })
 })
 
-//let btn = document.getElementById("like");
+// let newDiv = $("<div>").addClass("ftw")
 
-//btn.addEventListener("click", e=>{
-//let currentCount = btn.nextSibling.textContent.trim()
-//newCount = parseInt(currentCount)+1
-//btn.nextSibling.textContent = newCount
-//});
+// let btn = document.getElementById("like");
+
+// btn.addEventListener("click", e=>{
+// let currentCount = btn.nextSibling.textContent.trim()
+// newCount = parseInt(currentCount)+1
+// btn.nextSibling.textContent = newCount
+// });
