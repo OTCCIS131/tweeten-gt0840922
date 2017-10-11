03a6492f08ea9b3102aedc9b6e83de8e69ec9620
Garrett Troy <gt0840922@otc.edu>
2017-10-10 22:59:24 -0500

    My Update...again.

--- index.js
+++ index.js
@@ -1,11 +1,17 @@
-let btns = Array.from(document.getElementsByClassName('inc'))
-
-btns.forEach(btn => {
-    btn.addEventListener('click', e => {
-        console.log('click')
-        let currentCount = parseInt(btn.nextSibling.textContent)
-        let newCount = currentCount + 1
-
-        btn.nextSibling.textContent = ` ${newCount} `
+$("#fa fa-fw fa-heart inc").html(function(i, value) {return +val+1});
+// Your code goes here.
+    $("update").click(function() {
+        $("#fa fa-fw fa-heart inc").html(function(i, value) {return +val+1});
     })
-})
+
+//let btns = Array.from(document.getElementsByClassName('inc'))
+
+//btns.forEach(btn => {
+//    btn.addEventListener('click', e => {
+//        console.log('click')
+//        let currentCount = parseInt(btn.nextSibling.textContent)
+//        let newCount = currentCount + 1
+//
+//        btn.nextSibling.textContent = ` ${newCount} `
+//    })
+//})
